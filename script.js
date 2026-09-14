5138f4040019f1362df9d4ee63f61a58d7e87ba4
sennavdl <sca.vandelaar@student.avans.nl>
2026-09-14 20:31:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const myDiv7 = document.querySelector("#mijnDiv7");
+
+myDiv7.addEventListener("click", function () {
+  myDiv7.classList.toggle("red");
+});
+
+const hovertekst = document.querySelector("#hovertekst");
+
+hovertekst.addEventListener("mouseover", function()
+{hovertekst.textContent = "de tekst is veranderd!";});
+
+hovertekst.addEventListener("mouseout", function(){
+hovertekst.textcontent ="Ik ben Senna van de Laar en ik zit in leerjaar1.";});
+let myvar = 1;
+myvar
+
+
+
+
+
+
